124f8abb6e14790bf02b853da8bdda8282b3ef3c
Marie P. Lopez Cama <yaminelopezcama@gmail.com>
2021-03-24 04:17:43 +0100

    chat basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    SocketIoModule.forRoot(config)              //importar modulo de socketIO con la conf predeterminada
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  message = '';
+  messages = [];
+  currentUser = '';
 
-  constructor() {}
+  constructor(private socket: Socket, private toastCtrl: ToastController) { }  
 
+  ngOnInit() {
+    this.socket.connect();
+
+    let name = `user-${new Date().getTime()}`;    //random name basado en tiempo de entrada al chat
+    this.currentUser = name;
+
+    this.socket.emit('set-name', name);           //emitir al server (a todos los users) que has entrado
+
+    this.socket.fromEvent('users-changed').subscribe(data => {  //server devuelve el evento a traves de user-changed 
+      let user = data['user'];
+      if (data['event'] === 'left') {
+        this.showToast('User left: ' + user);
+      } else {
+        this.showToast('User joined: ' + user);
+      }
+    });
+
+    this.socket.fromEvent('message').subscribe(message => {   //recibir mensajes
+      this.messages.push(message);
+    });
+  }
+
+  sendMessage() {
+    this.socket.emit('send-message', { text: this.message });   //enviar mensaje al resto de users
+    this.message = '';                                          //vaciar textbox despues de enviar el mensaje
+  }
+
+  ionViewWillLeave() {
+    this.socket.disconnect();
+  }
+
+  async showToast(msg) {
+    let toast = await this.toastCtrl.create({
+      message: msg,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
 }
